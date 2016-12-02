2d9c38387fc1aab34489cc381cffaecf874cb11b
UnlikelyHeroTx <dvida3@wgu.edu>
2016-12-02 12:33:20 -0600

    Episode 21: Composer Autoloading
    Learned about Composer's Autoloading functionality and integrated it
    into the project.

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -5,11 +5,6 @@ $app =[];
 
 $app['config'] = require 'config.php';
 
-require 'core/router.php';
-require 'core/request.php';
-require 'core/database/connection.php';
-require 'core/database/querybuilder.php';
-
 $app['database'] = new QueryBuilder(
   Connection::make($app['config']['database'])
 );
